4cded3f89fa4695cb050de52a56652d51cf5d8ae
Michał Puchyr <michalpuchyr3@gmail.com>
2024-10-12 14:16:16 +0200

    Add Project table, fix Offer table

--- src/BHC24.Api/Persistence/Models/Offer.cs
+++ src/BHC24.Api/Persistence/Models/Offer.cs
@@ -2,7 +2,8 @@ namespace BHC24.Api.Persistence.Models;
 
 public class Offer : BaseTrackingEntity
 {
-    public string Title { get; set; } = null!;
-    public string Description { get; set; } = null!;
-    public ICollection<AppUser> Collaborators { get; set; } = [];
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required Investor Investor { get; set; }
+    public required Project Project { get; set; }
 }
