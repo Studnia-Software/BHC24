c8fed1ea71ed47811b005e34e8c4bffb87735632
sogod <140706370+SzymonKw7@users.noreply.github.com>
2024-10-12 18:19:55 +0200

    Investor endopints

---
+++ src/BHC24.Api/Models/Investors/Investor.cs
@@ -0,0 +1,17 @@
+﻿using BHC24.Api.Models.Projects;
+
+namespace BHC24.Api.Models.Investors;
+
+public class GetInvestorResponse
+{
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public string Surname { get; init; }
+    public string Email { get; init; }
+    public string PhoneNumber { get; init; }
+    public string? CompanyName { get; init; }
+    public bool HasPremium { get; set; } = false;
+    
+}
+
+public record AddInvestorRequest(string Name, string Surname, string Email, string PhoneNumber, string? CompanyName);
