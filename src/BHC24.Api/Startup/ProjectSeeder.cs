6788edbd6b6a59221a5212346bb1dc4bf4f8256a
sogod <140706370+SzymonKw7@users.noreply.github.com>
2024-10-12 16:04:27 +0200

    Add Offerts to ProjectSeeder

--- src/BHC24.Api/Startup/ProjectSeeder.cs
+++ src/BHC24.Api/Startup/ProjectSeeder.cs
@@ -20,6 +20,18 @@ public class ProjectSeeder
                 Description = "Description 1",
                 Owner = dbContext.AppUser.First(),
                 Collaborators = new ICollection<AppUser> { dbContext.AppUser.First() }
+				Offers = new ICollection<Offer> 
+				{ 
+						new Offer 
+								{ 
+									Title = "Offer 1",
+									Description = "Description 1",
+									Tags = new ICollection<Tag> 
+										{ 
+												 dbContext.Tags.FirstOrDefault(t => t.Name == "python") 
+										}
+								}
+				}
             },
             new()
             {
@@ -27,14 +39,48 @@ public class ProjectSeeder
                 Description = "Description 2",
                 Owner = dbContext.AppUser.First(),
                 Collaborators = new ICollection<AppUser> { dbContext.AppUser.First() }
-            },
+            	Offers = new ICollection<Offer> 
+				{ 
+						new Offer 
+								{ 
+									Title = "Offer 2",
+									Description = "Description 2",
+									Tags = new ICollection<Tag> 
+										{ 
+												 dbContext.Tags.FirstOrDefault(t => t.Name == "html"), 
+												 dbContext.Tags.FirstOrDefault(t => t.Name == "css") 
+								        }
+								} 
+				}
+			},
             new()
             {
                 Title = "Project 3",
                 Description = "Description 3",
                 Owner = dbContext.AppUser.First(),
                 Collaborators = new ICollection<AppUser> { dbContext.AppUser.First() }
-            }
+            	 Offers = new ICollection<Offer> 
+				{ 
+						new Offer 
+								{ 
+									Title = "Offer 3",
+									Description = "Description 3",
+									Tags = new ICollection<Tag> 
+										{ 
+												 dbContext.Tags.FirstOrDefault(t => t.Name == "html")
+										} 
+								},
+						new Offer 
+						{ 
+									Title = "Offer 4",
+									Description = "Description 4",
+									Tags = new ICollection<Tag> 
+										{ 
+												 dbContext.Tags.FirstOrDefault(t => t.Name == "javascript")
+										} 
+							}
+				}
+			}
         };
 
         dbContext.Project.AddRange(projects);
