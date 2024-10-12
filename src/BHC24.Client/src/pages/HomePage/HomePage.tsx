a82973429ca453131dfc76cceb7e421cb6dd8276
Damian Osiński <osinskidamian03@gmail.com>
2024-10-12 20:41:30 +0200

    Home page init

--- src/BHC24.Client/src/pages/HomePage/HomePage.tsx
+++ src/BHC24.Client/src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,13 @@
 import {JSX} from "react";
-import {Link} from "react-router-dom";
 import AnimatedMain from "../../components/AnimatedComps/AnimatedMain.tsx";
+import TitleSection from "./TitleSection/TitleSection.tsx";
+import AboutSection from "./AboutSection/AboutSection.tsx";
 
 function HomePage(): JSX.Element {
   return (
     <AnimatedMain>
-      <h1>Home Page</h1>
-      <p>Welcome to the Home Page!</p>
-      <Link to={'/user/aaa/info'}>TAK</Link>
+      <TitleSection/>
+      <AboutSection/>
     </AnimatedMain>
   );
 }
