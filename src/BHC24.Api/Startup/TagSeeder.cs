e5770df8fdd9ed9d7a967acd5badc93f956e062d
sogod <140706370+SzymonKw7@users.noreply.github.com>
2024-10-12 15:31:48 +0200

    Add TagSeeder

---
+++ src/BHC24.Api/Startup/TagSeeder.cs
@@ -0,0 +1,46 @@
+﻿using Api.Persistence;
+using Api.Persistence.Models;
+
+namespace Api.Startup;
+
+public class TagSeeder
+{
+    public static void Seed(BhcDbContext dbContext)
+    {
+        if (dbContext.Tag.Any())
+        {
+            return;
+        }
+
+        var tags = new Tag[]
+        {
+            new()
+            {
+                Name = "html"
+            }
+
+            new()
+            {
+                Name = "css"
+            }
+            
+            new()
+            {
+                Name = "javascript"
+            }
+            
+            new()
+            {
+                Name = "c#"
+            }
+            
+            new()
+            {
+                Name = "python"
+            }
+        };
+
+        dbContext.Tag.AddRange(tags);
+        dbContext.SaveChanges();
+    }
+}
