f8a1d3afb218565bdf176632e04d5e7f130f3adf
sogod <140706370+SzymonKw7@users.noreply.github.com>
2024-10-12 17:04:35 +0200

    Rebuild ProjectSeeder to give ProjectID for offers

--- src/BHC24.Api/Startup/ProjectSeeder.cs
+++ src/BHC24.Api/Startup/ProjectSeeder.cs
@@ -12,77 +12,86 @@ public class ProjectSeeder
             return;
         }
 
-        var projects = new Project[]
-        {
-            new()
-            {
-                Title = "Project 1",
-                Description = "Description 1",
-                Owner = dbContext.Users.First(),
-                Collaborators = new List<AppUser> { dbContext.Users.First() },
-				Offers = new List<Offer> 
+        var projects = new List<Project> { };
+        
+        projects.Add(
+	        new()
+	        {
+		        Title = "Project 1",
+		        Description = "Description 1",
+		        Owner = dbContext.Users.First(),
+		        Collaborators = new List<AppUser> { dbContext.Users.First() },
+		        Offers = new List<Offer>{}
+	        });
+        
+			projects[projects.Count-1].Offers.Add(
+				new Offer 
 				{ 
-						new Offer 
-								{ 
-									Title = "Offer 1",
-									Description = "Description 1",
-									Tags = new List<Tag> 
-										{ 
-												 dbContext.Tags.FirstOrDefault(t => t.Name == "python") 
-										}
-								}
-				}
-            },
-            new()
-            {
-                Title = "Project 2",
-                Description = "Description 2",
-                Owner = dbContext.Users.First(),
-                Collaborators = new List<AppUser> { dbContext.Users.First() },
-            	Offers = new List<Offer> 
-				{ 
-						new Offer 
-								{ 
-									Title = "Offer 2",
-									Description = "Description 2",
-									Tags = new ICollection<Tag> 
-										{ 
-												 dbContext.Tags.FirstOrDefault(t => t.Name == "html"), 
-												 dbContext.Tags.FirstOrDefault(t => t.Name == "css") 
-								        }
-								} 
-				}
-			},
-            new()
-            {
-                Title = "Project 3",
-                Description = "Description 3",
-                Owner = dbContext.Users.First(),
-                Collaborators = new List<AppUser> { dbContext.Users.First() },
-            	 Offers = new List<Offer> 
-				{ 
-						new Offer 
-								{ 
-									Title = "Offer 3",
-									Description = "Description 3",
-									Tags = new List<Tag> 
-										{ 
-												 dbContext.Tags.FirstOrDefault(t => t.Name == "html")
-										} 
-								},
-						new Offer 
+					Title = "Offer 1",
+					Description = "Description 1",
+					ProjectId = projects[projects.Count-1].Id,
+					Tags = new List<Tag> 
+					{ 
+						dbContext.Tags.FirstOrDefault(t => t.Name == "python") 
+					}
+				});
+				
+				projects.Add(
+						new()
+						{
+							Title = "Project 2",
+							Description = "Description 2",
+							Owner = dbContext.Users.First(),
+							Collaborators = new List<AppUser> { dbContext.Users.First() },
+							Offers = new List<Offer>{ }
+							
+						});
+				projects[projects.Count - 1].Offers.Add(
+					new Offer 
+					{ 
+						Title = "Offer 2",
+						Description = "Description 2",
+						ProjectId = projects[projects.Count - 1].Id,
+						Tags = new List<Tag> 
 						{ 
-									Title = "Offer 4",
-									Description = "Description 4",
-									Tags = new List<Tag> 
-										{ 
-												 dbContext.Tags.FirstOrDefault(t => t.Name == "javascript")
-										} 
-							}
-				}
-			}
-        };
+							dbContext.Tags.FirstOrDefault(t => t.Name == "html"), 
+							dbContext.Tags.FirstOrDefault(t => t.Name == "css") 
+						}
+					} );
+				
+				projects.Add(           
+					new()
+				{
+					Title = "Project 3",
+					Description = "Description 3",
+					Owner = dbContext.Users.First(),
+					Collaborators = new List<AppUser> { dbContext.Users.First() },
+					Offers = new List<Offer>{ }
+				});
 
+				projects[projects.Count - 1].Offers.Add(
+					new Offer
+					{
+						Title = "Offer 3",
+						Description = "Description 3",
+						ProjectId = projects[projects.Count - 1].Id,
+						Tags = new List<Tag>
+						{
+							dbContext.Tags.FirstOrDefault(t => t.Name == "html")
+						}
+					});
+				projects[projects.Count - 1].Offers.Add(
+					new Offer
+					{
+						Title = "Offer 4",
+						Description = "Description 4",
+						ProjectId = projects[projects.Count - 1].Id,
+						Tags = new List<Tag>
+						{
+							dbContext.Tags.FirstOrDefault(t => t.Name == "javascript")
+						}
+					});
+ 
         dbContext.Projects.AddRange(projects);
         dbContext.SaveChanges();
     }
