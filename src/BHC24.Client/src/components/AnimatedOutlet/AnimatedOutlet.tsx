fb3b89da2cff57f90a5ae872fc7560cf692144bf
Damian Osiński <osinskidamian03@gmail.com>
2024-10-12 17:35:30 +0200

    Add basic routing

---
+++ src/BHC24.Client/src/components/AnimatedOutlet/AnimatedOutlet.tsx
@@ -0,0 +1,17 @@
+import React, {JSX} from "react";
+import {useLocation, useOutlet} from "react-router-dom";
+import {AnimatePresence} from "framer-motion";
+
+function AnimatedOutlet(): JSX.Element {
+    const location = useLocation();
+    const element = useOutlet();
+
+    return (
+        <AnimatePresence mode="wait" initial={true}>
+            {element && React.cloneElement(element, {key: location.pathname})}
+        </AnimatePresence>
+    );
+}
+;
+
+export default AnimatedOutlet;
---
+++ src/BHC24.Client/src/pages/UserInfoPage/UserInfoPage.tsx
@@ -0,0 +1,12 @@
+import {JSX} from "react";
+import AnimatedMain from "../../components/AnimatedMain/AnimatedMain.tsx";
+
+function UserInfoPage(): JSX.Element {
+    return (
+        <AnimatedMain>
+            <h1>UserInfo</h1>
+        </AnimatedMain>
+    );
+}
+
+export default UserInfoPage;
