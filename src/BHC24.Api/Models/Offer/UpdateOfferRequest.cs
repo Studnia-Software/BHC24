26cbb0deaa8f6d880b80c81a29289146c966948f
Filip <filipkwiek888@gmail.com>
2024-10-12 16:08:58 +0200

    Create Offer endpoint

---
+++ src/BHC24.Api/Models/Offer/UpdateOfferRequest.cs
@@ -0,0 +1,12 @@
+using BHC24.Api.Persistence.Models;
+
+namespace BHC24.Api.Models.Offer;
+
+public class UpdateOfferRequest
+{
+    public required string Title { get; set; }
+    
+    public ICollection<AppUser> Collaborators { get; set; } = [];
+
+    public ICollection<Tag>? Tags { get; set; }
+}
