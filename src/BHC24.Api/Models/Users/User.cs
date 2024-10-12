dcb9c41cb22f401a3702aa5122b705337764a5b5
Filip <filipkwiek888@gmail.com>
2024-10-12 20:25:36 +0200

    Add User Endopoints
    # Conflicts:
    #	src/BHC24.Api/Migrations/BhcDbContextModelSnapshot.cs

---
+++ src/BHC24.Api/Models/Users/User.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BHC24.Api.Models.Users;
+
+public class GetUserResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public Persistence.Models.Profile Profile { get; set; }
+}
