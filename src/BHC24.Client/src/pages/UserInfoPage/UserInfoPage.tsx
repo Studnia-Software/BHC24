ee46e804d727ad88092e85fcbacce6994afff76c
Damian Osiński <osinskidamian03@gmail.com>
2024-10-12 20:41:30 +0200

    Home page init

--- src/BHC24.Client/src/pages/UserInfoPage/UserInfoPage.tsx
+++ src/BHC24.Client/src/pages/UserInfoPage/UserInfoPage.tsx
@@ -1,38 +1,19 @@
-import {JSX, useEffect, useState} from "react";
+import {JSX} from "react";
 import LoadingAnimation from "../../components/LoadingAnimation/LoadingAnimation.tsx";
-import {AnimatePresence} from "framer-motion";
 import AnimatedDiv from "../../components/AnimatedComps/AnimatedDiv.tsx";
+import AnimatedMain from "../../components/AnimatedComps/AnimatedMain.tsx";
+
+const user = null;
 
 function UserInfoPage(): JSX.Element {
-  const [user, setUser] = useState<null | object>(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setUser({
-        firstName: "John",
-        lastName: "Doe",
-        id_nickname: "@id_nickname",
-        email: "email@email.com",
-        github: "github_nick",
-        linkedin: "linkedin_nick",
-        phoneNumber: "123456789",
-        cv: "cv_link",
-        projects: [{}],
-        tags: [{}],
-        profilePicture: "profile_picture_link",
-        about: "about_text",
-      });
-    }, 2000);
-  }, []);
-
   return (
-    <AnimatePresence mode={"wait"}>
+    <AnimatedMain>
       {user ? (
         <AnimatedDiv key={"user"}><h2>User</h2></AnimatedDiv>
       ) : (
         <LoadingAnimation/>
       )}
-    </AnimatePresence>
+    </AnimatedMain>
   );
 }
 
