d01695058cefc6673d3f3ee364a7395fa4ed2228
sogod <140706370+SzymonKw7@users.noreply.github.com>
2024-10-12 14:56:33 +0200

    Delete Collabartors in offerts

--- src/BHC24.Api/Persistence/Models/Offer.cs
+++ src/BHC24.Api/Persistence/Models/Offer.cs
@@ -4,8 +4,6 @@ public class Offer : BaseTrackingEntity
 {
     public string Title { get; set; } = null!;
     public string Description { get; set; } = null!;
-    public ICollection<AppUser> Collaborators { get; set; } = [];
-
     public ICollection<Tag>? Tags { get; set; }
     public required Project Project { get; set; }
 }
