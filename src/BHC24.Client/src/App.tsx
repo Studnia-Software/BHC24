cb6d604cd4de0bb26a88d83550e0b22ad68eecea
Damian Osiński <82379588+kyzelm@users.noreply.github.com>
2024-10-13 05:47:11 +0200

    Css conflinct fixes (#58)
    * Auth pages styling
    
    * FIXIT: CSS conflicts

--- src/BHC24.Client/src/App.tsx
+++ src/BHC24.Client/src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Root from "./Root.tsx";
-import UserInfoPage from "./pages/UserInfoPage/UserInfoPage.tsx";
 import HomePage from "./pages/HomePage/HomePage.tsx";
 import {LoginPage} from './pages/Auth/Login/LoginPage.tsx';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {RegisterPage} from './pages/Auth/Register/RegisterPage.tsx';
 import {ProfilePage} from './pages/Profile/ProfilePage.tsx';
-import { ProjectSearch } from './pages/ProjectSearch/ProjectSearch.tsx';
+import {ProjectSearch} from './pages/ProjectSearch/ProjectSearch.tsx';
 
 const router = createBrowserRouter([
   {
@@ -40,16 +39,7 @@ const router = createBrowserRouter([
       },
       {
         path: 'projects',
-        element: <ProjectSearch />
-      },
-      {
-        path: 'user/:id',
-        children: [
-          {
-            path: 'info',
-            element: <UserInfoPage/>
-          }
-        ]
+        element: <ProjectSearch/>
       }
     ]
   }
--- src/BHC24.Client/src/pages/HomePage/HomePage.tsx
+++ src/BHC24.Client/src/pages/HomePage/HomePage.tsx
@@ -2,8 +2,6 @@ import {JSX} from "react";
 import AnimatedMain from "../../components/AnimatedComps/AnimatedMain.tsx";
 import TitleSection from "./TitleSection/TitleSection.tsx";
 import AboutSection from "./AboutSection/AboutSection.tsx";
-import {Link} from "react-router-dom";
-import AnimatedMain from "../../components/AnimatedComps/AnimatedMain.tsx";
 
 function HomePage(): JSX.Element {
   return (
