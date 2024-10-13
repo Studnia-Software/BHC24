097ea74a4cc1eec5996d4b8fa217f3ed17ac54c1
Damian Osiński <osinskidamian03@gmail.com>
2024-10-13 08:50:52 +0200

    Add title page icon

--- src/BHC24.Client/src/pages/HomePage/TitleSection/TitleSection.tsx
+++ src/BHC24.Client/src/pages/HomePage/TitleSection/TitleSection.tsx
@@ -3,12 +3,14 @@ import styles from "./TitleSection.module.css";
 import BlurBg from "../../../components/BlurBg/BlurBg.tsx";
 import DisplayButton from "../../../components/Buttons/DisplayButton.tsx";
 import {useNavigate} from "react-router-dom";
+import logo from '/public/logo.png';
 
 function TitleSection(): JSX.Element {
   const navigate = useNavigate()
 
   return (
     <section className={styles.titleSection}>
+      <img src={logo} alt="logo"/>
       <div className={styles.textCon}>
         <BlurBg/>
         <h1><span className={"text"}>Twoje projekty</span><br/>Twoja kariera</h1>
