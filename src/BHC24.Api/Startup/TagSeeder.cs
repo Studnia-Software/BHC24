bcdeff3963405fd440d4313740457ba4b7957821
sogod <140706370+SzymonKw7@users.noreply.github.com>
2024-10-13 09:25:08 +0200

    new Icon location in seeders

--- src/BHC24.Api/Startup/TagSeeder.cs
+++ src/BHC24.Api/Startup/TagSeeder.cs
@@ -17,39 +17,39 @@ public class TagSeeder
             new()
             {
                 Name = "html",
-                ImagePath = "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png"
+                ImagePath = "src//img//html.png"
             },
 
             new()
             {
                 Name = "css",
-                ImagePath = "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png"
+                ImagePath = "src//img//css.png"
             },
             
             new()
             {
                 Name = "javascript",
-                ImagePath = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Javascript-shield.svg"
+                ImagePath = "src//img//javascript.png"
             },
             new()
             {
                 Name = "c#",
-                ImagePath = "https://upload.wikimedia.org/wikipedia/commons/1/17/C_Sharp_Icon.png"
+                ImagePath = "src//img//csharp.png"
             },
             new()
             {
                 Name = "java",
-                ImagePath = "https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg"
+                ImagePath = "src//img//java.png"
             },
             new()
             {   
                 Name ="python",
-                ImagePath = "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg"
+                ImagePath = "src//img//python.png"
             },
             new()
             {
                 Name = "php",
-                ImagePath = "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg"
+                ImagePath = "src//img//php.png"
             }
         };
 
