583666f2ff3c838572db91ea74b7b74e5fa7f7ed
Damian Osiński <82379588+kyzelm@users.noreply.github.com>
2024-10-13 09:00:04 +0200

    Title section logo (#63)
    * Auth pages styling
    
    * FIXIT: CSS conflicts
    
    * Change logo
    
    * Change title
    
    * Add title page icon

--- src/BHC24.Client/src/pages/HomePage/HomePage.tsx
+++ src/BHC24.Client/src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,13 @@
 import {JSX} from "react";
 import AnimatedMain from "../../components/AnimatedComps/AnimatedMain.tsx";
 import TitleSection from "./TitleSection/TitleSection.tsx";
-import AboutSection from "./AboutSection/AboutSection.tsx";
+// import AboutSection from "./AboutSection/AboutSection.tsx
 
 function HomePage(): JSX.Element {
   return (
     <AnimatedMain>
       <TitleSection/>
-      <AboutSection/>
+      {/*<AboutSection/>*/}
     </AnimatedMain>
   );
 }
