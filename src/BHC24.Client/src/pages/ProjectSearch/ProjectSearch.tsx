ead300702847f9338b8a03103245a575c1031c3c
Damian Osiński <82379588+kyzelm@users.noreply.github.com>
2024-10-13 10:20:43 +0200

    Kyzelm front/styling projects (#65)
    * Project styling
    
    * w sumie to nie pamiętam

--- src/BHC24.Client/src/pages/ProjectSearch/ProjectSearch.tsx
+++ src/BHC24.Client/src/pages/ProjectSearch/ProjectSearch.tsx
@@ -1,22 +1,24 @@
-import { useEffect, useState } from "react";
-import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {useEffect, useState} from "react";
+import {faAngleLeft, faAngleRight} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import AnimatedMain from "../../components/AnimatedComps/AnimatedMain";
-import { Divider } from "../../components/Divider/Divider";
-import { useGetProjects } from "../../hooks/useGetProjects";
-import { GetTagResponse, PaginationRequest } from "../../utils/models";
-import { isLastPage } from "../../utils/helpers";
+import {Divider} from "../../components/Divider/Divider";
+import {useGetProjects} from "../../hooks/useGetProjects";
+import {GetTagResponse, PaginationRequest} from "../../utils/models";
+import {isLastPage} from "../../utils/helpers";
 import styles from './ProjectSearch.module.css';
-import { useGetTags as useGetTags } from "../../hooks/useGetTags";
+import {useGetTags as useGetTags} from "../../hooks/useGetTags";
+import AnimatedDiv from "../../components/AnimatedComps/AnimatedDiv.tsx";
+import {AnimatePresence} from "framer-motion";
 
 export function ProjectSearch() {
-  const [pagination, setPagination] = useState<PaginationRequest>({ page: 1, pageSize: 10 });
+  const [pagination, setPagination] = useState<PaginationRequest>({page: 1, pageSize: 10});
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [selectedTags, setSelectedTags] = useState<GetTagResponse[]>([]);
   const [ownerName, setOwnerName] = useState<string>('');
-  const { data: tags } = useGetTags();
+  const {data: tags} = useGetTags();
 
-  const { data: projects, isLoading, error, refetch } = useGetProjects({
+  const {data: projects, isLoading, error, refetch} = useGetProjects({
     projectName: searchQuery,
     tags: selectedTags.map(t => t.name),
     ownerName: ownerName
@@ -24,7 +26,7 @@ export function ProjectSearch() {
 
   useEffect(() => {
     refetch();
-  }, [searchQuery, pagination, tags, ownerName]);
+  }, [searchQuery, pagination, tags, ownerName, refetch]);
 
   const TagBlock = ({ tag }: { tag: GetTagResponse }) => (
     <div style={{
@@ -42,13 +44,14 @@ export function ProjectSearch() {
   );
 
   const SearchResults = () => (
-    <div style={{ width: '100%' }}>
+    <div style={{width: '100%'}}>
       {isLoading && searchQuery !== '' && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
       {projects && searchQuery !== '' && projects.data?.data.map((project, index) => (
-        <div key={index} style={{ marginBlock: '1rem' }}>
-          <h3 style={{ color: 'var(--color-text)' }}>{project.title}</h3>
+        <div key={index} style={{marginBlock: '1rem'}}>
+          <h3 style={{color: 'var(--color-text)'}}>{project.title}</h3>
           <p>{project.description}</p>
+
           <p>Właściciel: <a href='#' style={{ textDecoration: 'underline' }}>{project.owner}</a></p>
           <p>Współpracownicy: {project.collaboratorsCount}</p>
           <div style={{ display: 'flex', gap: '0.5rem', marginBlock: '1rem' }}>
@@ -63,19 +66,20 @@ export function ProjectSearch() {
   );
 
   const PaginationInput = () => (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '2rem' }}>
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '2rem'}}>
       <button
-        onClick={() => setPagination({ ...pagination, page: pagination.page - 1 })}
+        onClick={() => setPagination({...pagination, page: pagination.page - 1})}
         className={styles.paginationArrow}
         disabled={pagination.page === 1}>
-        <FontAwesomeIcon icon={faAngleLeft} />
+        <FontAwesomeIcon icon={faAngleLeft}/>
       </button>
-      <div>{pagination.page}</div>
+      <AnimatePresence mode={"wait"}><AnimatedDiv
+        key={pagination.page}>{pagination.page}</AnimatedDiv></AnimatePresence>
       <button
-        onClick={() => setPagination({ ...pagination, page: pagination.page + 1 })}
+        onClick={() => setPagination({...pagination, page: pagination.page + 1})}
         disabled={projects?.data == null ? false : isLastPage(projects?.data)}
         className={styles.paginationArrow}>
-        <FontAwesomeIcon icon={faAngleRight} />
+        <FontAwesomeIcon icon={faAngleRight}/>
       </button>
     </div>
   );
@@ -84,24 +88,21 @@ export function ProjectSearch() {
     <AnimatedMain>
       <h2>Wyszukiwarka projektów</h2>
       <input type="text"
-        onChange={(e) => {
-          setSearchQuery(e.target.value)
-          setPagination({ page: 1, pageSize: 10 });
-        }}
-        style={{
-          backgroundColor: 'var(--color-grey)',
-          padding: '0.5rem',
-          fontSize: '1.5rem',
-          color: 'var(--color-background)'
-        }} />
-      <div style={{ width: '100%' }}>
-        <h3 style={{ textAlign: 'center', marginBottom: '1rem' }}>Filtry</h3>
-        <Divider />
+             placeholder={'Wyszukaj projekt...'}
+             onChange={(e) => {
+               setSearchQuery(e.target.value)
+               setPagination({page: 1, pageSize: 10});
+             }}/>
+      <div style={{width: '100%'}}>
+        <h3 style={{textAlign: 'center', marginBottom: '1rem'}}>Filtry</h3>
+        <Divider/>
         <div>
-          <label>Tagi</label>
           <div style={{
             display: "flex",
-            gap: '0.25rem',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '3rem',
+            marginTop: '3rem',
           }}>
             {tags && tags.data?.map((tag) => (
               <div key={tag.name}
@@ -128,17 +129,17 @@ export function ProjectSearch() {
             ))}
           </div>
         </div>
-        <div>
-          <label>Właściciel</label>
-          <input type="text" id="ownerName" onChange={(e) => setOwnerName(e.target.value)} />
-        </div>
-        <div>
-          <label>Współpracownicy</label>
-          <input type="text" id="collaborators" />
-        </div>
+        {/*<div>*/}
+        {/*  <label>Właściciel</label>*/}
+        {/*  <input type="text" id="ownerName" onChange={(e) => setOwnerName(e.target.value)}/>*/}
+        {/*</div>*/}
+        {/*<div>*/}
+        {/*  <label>Współpracownicy</label>*/}
+        {/*  <input type="text" id="collaborators"/>*/}
+        {/*</div>*/}
       </div>
-      <SearchResults />
-      <PaginationInput />
+      <SearchResults/>
+      <PaginationInput/>
     </AnimatedMain>
   );
 }
